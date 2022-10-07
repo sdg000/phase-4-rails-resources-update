205355827ee68086f12ddd590378c72b20a9d286
Seth David Gyimah <esec26@gmail.com>
2022-10-07 12:00:48 +0000

    completed code along requirements

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -22,10 +22,48 @@ class BirdsController < ApplicationController
     end
   end
 
+  #patch /bird/:id
+  def update
+    #find instance
+    bird = Bird.find_by(id:params[:id])
+
+    #if found, update instance
+    if bird
+      bird.update(bird_params)
+
+      #return instance in json format
+      render json: bird, status: :created
+    else
+      render json: {error: "data not found"}, status: :not_found
+  end
+
+  # breaking RESTFUL CONVENTIONS by adding custom action/method to handle "likes" button click action.
+
+  # BEST PRACTICE, CREATE DIFFERENT CONTROLLER FOR CUSTOM ROUTES
+  # http://jeromedalbert.com/how-dhh-organizes-his-rails-controllers/
+  def increment_likes
+
+      #find bird instance
+      bird = Bird.find_by(id:params[:id])
+
+      #if found, increment likes by 1
+      if bird
+        bird.update(likes: bird.likes + 1)
+        
+        #return instance in json format
+        render json: bird
+      else
+        render json: {error: "data not found"}, status: :not_found
+      end
+  end
+
   private
 
   def bird_params
-    params.permit(:name, :species)
+    # params.permit(:name, :species)
+
+    #include :likes to allowed parameters
+    params.permit(:name, :species, :likes)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show, :create]
+  resources :birds, only: [:index, :show, :create, :update]
+
+  # breaking RESTFUL CONVENTIONS by adding custom route to handle "likes" button click action.
+  # BEST PRACTICE, CREATE DIFFERENT CONTROLLER FOR CUSTOM ROUTES
+  # http://jeromedalbert.com/how-dhh-organizes-his-rails-controllers/
+
+  patch "/birds/:id/like", to: "birds#increment_likes"
 end
